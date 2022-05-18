71be66f3dc6b58b8420c53ef1e1f4bf7e312785f
rantyd <46378155+rantyd@users.noreply.github.com>
2022-05-18 09:26:20 +0300

    Add files via upload

---
+++ multi_company_category/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2015-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Category multi-company",
+    "summary": "Select individually the public category visibility on each " "company",
+    "author": "Hezekia",
+    "category": "Product Management",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["website"],
+    "data": ["views/public_category_form.xml",
+             "views/public_category_tree.xml",
+             "security/ir_rule.xml"],
+}
